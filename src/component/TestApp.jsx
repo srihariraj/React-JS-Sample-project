f5fe63d980b44b5d3364fc0447473ca8f2bde064
srihariraj <srihariatom@gmail.com>
2025-08-07 23:31:18 +0530

    test app first commit

---
+++ src/component/TestApp.jsx
@@ -0,0 +1,10 @@
+function TestApp(){
+    return (
+        <div>
+        <h1>Test Application</h1>
+        <p>This is a simple test application.</p>
+        </div>
+    );
+}
+
+export default TestApp;
